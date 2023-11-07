7026a2838ee60b40d1ce36b1dea83fd7fdc08a5e
mamiprogramming <mamiprogramming@gmail.com>
2023-11-07 16:20:01 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,54 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except:[:index,:new]
+  before_action :contributor, only: [:edit, :update, :destroy] 
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+       redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    prototype = Prototype.find(params[:id])
+    if prototype.update(prototype_params)
+       redirect_to prototypes_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def contributor
+    redirect_to root_path unless current_user == @prototype.user
+  end
+end
