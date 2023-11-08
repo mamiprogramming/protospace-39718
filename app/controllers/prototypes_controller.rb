f7199aa6b01eeb103eeab553fc3dc63b62c417a6
mamiprogramming <mamiprogramming@gmail.com>
2023-11-08 22:36:39 +0900

    4コミット

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,7 +33,7 @@ class PrototypesController < ApplicationController
   def update
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
-       redirect_to prototypes_path(@prototype)
+       redirect_to prototype_path(@prototype)
     else
       render :edit
     end
